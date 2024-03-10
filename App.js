254e075c73da3580fe6be1b6843326d42764add3
raishivamm <shrai0432@gmail.com>
2024-03-10 07:19:20 +0530

    Building nested type of structure

--- App.js
+++ App.js
@@ -1,3 +1,34 @@
-const heading = React.createElement("h1", {}, "Hello World from React!");
+// When We have to use different type of tags:-
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h1", {}, "I am an h1 tag")
+  )
+);
+
+// Create a sibling of h tags:- We have to use array for this type of creation.
+const parent1 = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am an h1 tag"),
+    React.createElement("h2", {}, "I am an h2 tag"),
+  ])
+);
+
+// Create two div elements a child1 and the another child2.
+const parent2 = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am an h1 tag"),
+    React.createElement("h2", {}, "I am an h2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I am an h1 tag"),
+    React.createElement("h2", {}, "I am an h2 tag"),
+  ]),
+]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(parent2);
